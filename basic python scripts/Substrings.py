114367d6e1bb0b698ea262bb586bb4e378fc3490
Kiran S Baliga <60563196+kiransbaliga@users.noreply.github.com>
2020-10-01 23:46:38 +0530

    Update Substrings.py

--- basic python scripts/Substrings.py
+++ basic python scripts/Substrings.py
@@ -1,6 +1,6 @@
 #Easy python script to generate all the substringss of a given string
-test_str='HELLO'
+teststring='HELLO'
 #res is a list(list-->[])
-res = [test_str[i: j] for i in range(len(test_str))                        #splits the  test_str and copies it into result as list
-          for j in range(i + 1, len(test_str) + 1)] 
+res = [teststring[i: j] for i in range(len(teststring))                        #splits the  test_str and copies it into result as list
+          for j in range(i + 1, len(teststring) + 1)] 
 print(res)
