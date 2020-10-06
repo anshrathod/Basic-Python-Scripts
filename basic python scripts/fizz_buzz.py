746244868cc56826bb05c04c168f75b60723ab07
jungrishi <ranabhat.85@gmail.com>
2020-10-06 20:09:15 +0545

    Add doc strings to the scripts

--- basic python scripts/fizz_buzz.py
+++ basic python scripts/fizz_buzz.py
@@ -1,3 +1,13 @@
+""" Prints Fizz when number divisible by Three.
+    Prints Buzz when number divisible by Five.
+    Prints FizzBuzz when number divisible by Three and Five.
+    Else Prints the Actual Number
+
+    Example: if Number = 3 Then Fizz
+             if Number = 5 Then Buzz
+             if Number = 15 Then FizzBuzz
+             If Number = 7 Then 7
+"""
 def fizz_buzz(num):
     output = ""
     if num % 3 == 0:
@@ -12,4 +22,4 @@ for i in range(1, 100):
     fizz_buzz(i)
 
 # List Comprehension
-# [fizz_buzz(i) for i in range(1, 100)] 
+# [fizz_buzz(i) for i in range(1, 100)]
