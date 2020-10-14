90a08f3c8edcc705307bcdb7e1a2dbb62605e1da
Saman Ebrahimnezhad <ebsa491@gmail.com>
2020-10-14 12:21:50 +0330

    The script skel was added

---
+++ Web-Scraping/covid_cli/covid_cli.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# Created by Sam Ebison ( https://github.com/ebsa491 )
+# If you have found any important bug or vulnerability,
+# contact me pls, I love learning ( email: ebsa491@gmail.com )
+
+"""
+A script for extracting covid-19 virus online information from (https://www.worldometers.info/coronavirus/)
+"""
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    """The main function of the script."""
+    pass
+
+def send_request(url):
+    """
+    Sends a GET request to the URL,
+    and returns the result.
+    - url: The website URL.
+    """
+    pass
+
+def parse_result(result):
+    """
+    Parses the HTML result,
+    and returns the info.
+    - result: The HTML result (send_request function returns).   
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
