8ef02b21545b1387d4282de0396a1845eaa199eb
rsgsaad <63123740+rsgsaad@users.noreply.github.com>
2020-10-16 09:13:25 +0530

    Update LinearSearch.py
    linearSearch should work on unsorted list

--- LinearSearch.py
+++ LinearSearch.py
@@ -6,4 +6,25 @@ def sortedSearch(A, v):
             return -1
     return -1
 
+
+
+
+
 sortedSearch([1,2,3,5,6,9,12],6)
+
+
+# Above code complexity will be O(n) if  List is  sorted 
+
+
+# Linear Search Complexity should  O(n) whether it's sorted or not
+
+
+def linearSearch(mylist,key):
+    for index in range(len(mylist)):
+        if mylist[index]==key:
+            return index
+    return -1
+
+
+
+print(linearSearch([1,2,3,5,6,9,12],6))
