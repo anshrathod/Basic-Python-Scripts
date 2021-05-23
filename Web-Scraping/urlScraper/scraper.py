0227012db48a40c20dee49dceec216ee24e1f8fd
Nilay Neerav <neeravnilay@gmail.com>
2021-05-23 15:29:00 +0530

    new script added

---
+++ Web-Scraping/urlScraper/scraper.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+
+# creating empty list
+urls = []
+ 
+# function created
+
+def Scrape(site):
+    # getting the request from url
+    r = requests.get(site)
+
+    # converting the text
+
+    s = BeautifulSoup(r.text,"html.parser")
+    for i in s.find_all("a"):
+        href = i.attrs['href']
+        if href.startswith("/"):
+            site = site + 'href'
+            if site not in urls:
+                urls.append(site)
+                print(site)
+
+                # calling function via recursion
+                Scrape(site)
+
+# main function
+if __name__ == "__main__":
+    site = "https://www.linkedin.com//"
+    Scrape(site)
