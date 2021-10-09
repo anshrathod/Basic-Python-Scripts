d63e0843172fb40a649358ff4cb5acbbae2e2d16
Rohit Singh <RohitSinghSays21@gmail.com>
2021-10-10 01:01:12 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,9 +1,8 @@
-# example of a recursive function.
 def factorial(num):
-    if num <= 1:
+    if num ==0 or num==1:
         return 1
     else:
-        result = num * factorial(num - 1)
-        return result
-
-print(factorial(7))
+        return num * factorial(num - 1)
+    
+n=int(input("Enter a Non-Negative Integer : "))
+print(n,"! is ",factorial(n),sep="")
