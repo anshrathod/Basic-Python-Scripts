61258ca111da54228feade7af704652e949606b0
ARCHANA L <75906731+Archana-L-2000@users.noreply.github.com>
2021-10-24 12:34:51 +0530

    Added Redscreening file
    This code modifies the red color in file1 to the parts of the image provided in the file2

--- Image Processing/Redscreening.py
+++ Image Processing/Redscreening.py
@@ -1,8 +1,8 @@
-from simpleimage import SimpleImage # simpleimage library have to be installed (if not working properly just get the source code from github)
+from simpleimage import SimpleImage # simpleimage library have to be installed (if not working properly just get the source code from github) from here
 INTENSITY_THRESHOLD = 1.6
 
-#converting the filename1 image to red screen image
-
+#This code fills the 'red color' in 'filename1' file to the 'parts' in 'filename2' file
+#Note:'filename1' file should contain the image with red colored parts 
 def redscreen(filename1, filename2):
     Image = SimpleImage(filename1)
     back = SimpleImage(filename2)
@@ -15,14 +15,18 @@ def redscreen(filename1, filename2):
     return Image
 
 #driver code
+
 def main():
     orginal_stop = SimpleImage("filename1")
-    #orginal_stop.show()
+    orginal_stop.show()#shows file1
 
     back_img = SimpleImage("filename2")
-    #back_img.show()
+    back_img.show()#shows file2
     new_image = redscreen("filename1", "filename2")
-    new_image.show()
+    new_image.show()#shows the output of file1 and file2
 
 
-main()
+
+if __name__ == '__main__':
+    main()
+
