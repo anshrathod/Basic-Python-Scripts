3d1595af584da14a8e07350cb8126b08b614a471
root <smit16@somaiya.edu>
2022-09-30 07:23:38 +0000

    feat: added port scanning python script

--- Utils/port_scanner.py
+++ Utils/port_scanner.py
@@ -19,10 +19,10 @@ print(ascii_banner)
 
 # Defining a target
 if len(sys.argv) == 2:
-	# Translate hostname to IPv4 Address
-	target = socket.gethostbyname(sys.argv[1])
+    # Translate hostname to IPv4 Address
+    target = socket.gethostbyname(sys.argv[1])
 else:
-	print("Invalid amount of Argument")
+    print("Invalid amount of Argument")
 
 print(" -" * 50)
 print(" Scanning Target: " + target)
@@ -30,23 +30,23 @@ print(" Scanning started at: " + str(datetime.now()))
 print(" -" * 50)
 
 try:
-	# Recursively scan ports between 1 to 65,535
-	for port in range(1, 65535):
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		socket.setdefaulttimeout(1)
+    # Recursively scan ports between 1 to 65,535
+    for port in range(1, 65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
 		
-		# Returns an error indicator
-		result = s.connect_ex((target,port))
-		if result == 0:
-			print("\nPort {} is open.".format(port))
-		s.close()
+        # Returns an error indicator
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print("\nPort {} is open.".format(port))
+        s.close()
 		
 except KeyboardInterrupt:
-		print("\n Exiting Program.")
-		sys.exit()
+    print("\n Exiting Program.")
+    sys.exit()
 except socket.gaierror:
-		print("\n Hostname Could Not Be Resolved. Please try again with other Hostname.")
-		sys.exit()
+    print("\n Hostname Could Not Be Resolved. Please try again with other Hostname.")
+    sys.exit()
 except socket.error:
-		print("\n Server not responding. Please try again after sometime.")
-		sys.exit()
+    print("\n Server not responding. Please try again after sometime.")
+    sys.exit()
